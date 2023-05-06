f5a492bd7f482e0fd2874e987b205bfbf0447ce5
lt <lt1465956992@sina.com>
2023-05-06 09:46:50 +0800

    lt master first modify

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -8,8 +8,7 @@ public class App {
         System.out.println("Hello Git!lt hotfix first commit!");
         System.out.println("Hello Git!lt hotfix first modify!");
         System.out.println("Hello Git!lt master first modify!");
-        System.out.println("Hello Git!lt hotfix second modify!");
-        System.out.println("Hello Git!lt hotfix third modify!");
+
 
     }
 }
