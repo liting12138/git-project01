7e937a83d280776dd464890c786fb2cbe0f5a658
lt <lt1465956992@sina.com>
2023-05-06 09:06:20 +0800

    lt hotfix first commit

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -5,5 +5,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git!lt master second commit!");
+        System.out.println("Hello Git!lt hotfix first commit!");
+
     }
 }
